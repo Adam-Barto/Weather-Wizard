21093a57feb266f2134e1509bcec5e3bcf7d0be7
Adam-Barto <adam.jbarto@gmail.com>
2023-05-13 14:13:58 -0400

    Submit Button Now Hooked Up

--- static/js/index.js
+++ static/js/index.js
@@ -13,13 +13,16 @@ control.to_display.textContent = control.Month(control.to_slider.value);
 //Create Connections
 control.from_slider.addEventListener("input", (event) => {
     control.from_display.textContent = control.Month(control.from_slider.value)
-    build_data()
     });
 
 control.to_slider.addEventListener("input", (event) => {
     control.to_display.textContent = control.Month(control.to_slider.value)
-    build_data()
     });
+
+document.getElementById("submit").addEventListener("click", (event) => {
+        build_data()
+});
+
 //Create Back to Front
 var map = L.map('map').setView([38, -98], 4);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -27,27 +30,39 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+var marker = null;
 function onMapClick(e) {
 //    alert("You clicked the map at " + e.latlng);
-    build_data(e.latlng);
-//    alert(typeof(e.latlng))
+    if (marker == null) {
+        marker = new L.marker(e.latlng).addTo(map);
+
+    } else {
+        map.removeLayer(marker)
+        marker = new L.marker(e.latlng).addTo(map);
     }
+
+//    alert(typeof(e.latlng))
+}
 
 map.on('click', onMapClick);
 
 
-function build_data(target_point=null) {
+function build_data() {
     let from = control.from_display.textContent
     let to = control.to_display.textContent
-    let data = {
-        'from' : from,
-        'to' : to,
-        'lat' : target_point.lat,
-        'lng' : target_point.lng
+    if (marker) {
+        let target_point = marker.getLatLng()
+        let data = {
+            'from' : from,
+            'to' : to,
+            'lat' : target_point.lat,
+            'lng' : target_point.lng
+            }
+            let transfer = new Transfer()
+            transfer.sendData(data)
     }
-    let transfer = new Transfer()
-    transfer.sendData(data)
-//    grab_data(transfer)
+
+    //    grab_data(transfer)
 }
 
 
