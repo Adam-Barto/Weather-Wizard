8c57ad0a2efb7c57df874ff0ee4ab5f357a58738
polecarozc <zcpoelcaro@gmail.com>
2023-05-13 19:25:14 -0400

    changed function in logic to allow for data to come from front end

--- logic.py
+++ logic.py
@@ -4,6 +4,7 @@ from geopy.exc import GeocoderTimedOut
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.ensemble import RandomForestClassifier
+
 class df:
     def __init__(self):
         self.df = pd.read_csv('data/us_tornado_dataset_1950_2021.csv')
@@ -11,9 +12,9 @@ class df:
         self.state = ''
         self.lon = 0.0
         self.lat = 0.0
-        self.month = 9
-        self.day = 15
-        
+        self.month = 0
+        self.day = 1
+
     def show_df(self):
         print(self.df)
 
@@ -33,9 +34,9 @@ class df:
                 print(f'Timed out after {current_attempt} call(s), retrying')
         raise Exception('FAILED AFTER 3 CALLS')
 
-    def get_odds(self):
-        filtered_df = self.df[(self.df['slat']>=self.lat -1) & (self.df['slon']<= self.lat + 1)]
-        filtered_df2 = self.df[(self.df['mo']>=self.month - 1) & (self.df['mo']<= self.month + 1)]
+    def get_odds(self, to_month, from_month, lat, lon):
+        filtered_df = self.df[(self.df['slat']>=lat - 1) & (self.df['slon']<= lon + 1)]
+        filtered_df2 = self.df[(self.df['mo']>=to_month) & (self.df['mo']<= from_month)]
         total_filtered = len(filtered_df + filtered_df2)
         total_df = len(self.df)
         probability = total_filtered/total_df
